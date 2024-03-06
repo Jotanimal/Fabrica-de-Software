f91ca2018b22aee28d45d7b5685f3db2e1f8b9d1
gugazfxd <joaoaguiar.fn@gmail.com>
2024-03-06 16:36:14 -0300

    Update do desafio 4
    Modificando o código do quarto desafio da fábrica

--- Dia 1/Desafio4.py
+++ Dia 1/Desafio4.py
@@ -1,22 +1,39 @@
+# Criando a classe Animal
+
 class Animal():
     def __init__(self, nome, raca, cor):
         self.nome = nome
         self.raca = raca
         self.cor = cor
 
+# Definindo as hierarquias da classe Animal (Cachorro e Gato)
+
 class Cachorro(Animal):
     def dados(self):
-        print("Seu cachorrro se chama", self.nome, "é", self.raca, "e é", self.cor)
+        print("Seu cachorrro se chama {}. É {}. E é {}.".format(self.nome, self.raca, self.cor)) # Utilizando o str.format
     def som(self):
         print("Woof!!")
 
 class Gato(Animal):
     def dados(self):
-        print("Seu gato se chama", self.nome, "é", self.raca, "e é", self.cor)
+        print("Seu gato se chama %s." % self.nome, "É %s." % self.raca, "E é %s." % self.cor) # Utilizando o método de ponteiros de C (%s)
     def som(self):
         print("Meown!!")
 
-meu_pet = Cachorro("Pepe", "Vira-Lata", "Preto e Branco")
+tipo_animal = input("Você tem um Cachorro ou Gato? \n")
+
+# Condicional para receber input do usuário
+
+if(tipo_animal == "Cachorro"):
+    nome = input("Digite o nome do seu cachorro: ")
+    raca = input("Digite a raça do seu cachorro: ")
+    cor = input("Digite a cor do seu cachorro: ")
+    meu_pet = Cachorro(nome, raca, cor)
+elif(tipo_animal == "Gato"):
+    nome = input("Digite o nome do seu gato: ")
+    raca = input("Digite a raça do seu gato: ")
+    cor = input("Digite a cor do seu gato: ")
+    meu_pet = Gato(nome, raca, cor)
 
 meu_pet.dados()
 meu_pet.som()
